1c75ae18ad833f35da0acc48425f3d4130b2ef82
avakill <nobinel.suarez@gmail.com>
2013-12-11 00:57:44 +0200

    edited testingclass1.java

--- src/bellbutton/TestingClass1.java
+++ src/bellbutton/TestingClass1.java
@@ -1,5 +1,14 @@
 package bellbutton;
 
-public class TestingClass1 {
+import org.junit.Test;
 
+import junit.framework.TestCase;
+
+public class TestingClass1 extends TestCase
+{
+	@Test
+	public void testSomething()
+	{
+		
+	}
 }
